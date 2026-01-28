d00b3ed08decb71cb59beafafee1c5703212d899
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2026-01-28 18:56:06 -0300

    Deploy do projeto crm grupo 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,26 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { ProdutoModule } from './produtos/produto.module';
-import { AppService } from './app.service';
-import { Produto } from './produtos/entities/produto.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
-import { Usuario } from './usuario/entities/usuario.entity';
-import { Categoria } from './categoria/entities/categoria.entity';
 import { CategoriaModule } from './categoria/categoria.module';
+import { ProdService } from './data/prod.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'db_crm',
-        entities: [Produto,Usuario,Categoria],
-        synchronize: true,
-}),  
+    ConfigModule.forRoot(),
+      TypeOrmModule.forRootAsync({
+        useClass: ProdService,
+          imports: [ConfigModule],
+      }),
     AuthModule,
     UsuarioModule,
     ProdutoModule,
---
+++ src/data/dev.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+import { Categoria } from "../categoria/entities/categoria.entity";
+import { Produto } from "../produtos/entities/produto.entity";
+import { Usuario } from "../usuario/entities/usuario.entity";
+
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'root',
+            database: 'db_crm',
+            entities: [Categoria, Produto, Usuario],
+            synchronize: true,
+    };
+  }
+}
